9fb4dfdc5f66810fd54c56f90e13a084ef7e5f2e
tqa1308 <25023159@vnu.edu.vn>
2026-04-28 02:02:18 +0700

    SỬa tính toán code, tạo lỗi 4

--- src/test/java/com/lab/ShippingCalculatorTest.java
+++ src/test/java/com/lab/ShippingCalculatorTest.java
@@ -9,7 +9,7 @@ public class ShippingCalculatorTest {
 
     @Test
     void testStandard() {
-        assertEquals(15000.0, calc.calculate(5, "STANDARD"));
+        assertEquals(99999.0, calc.calculate(5, "STANDARD"));
     }
 
     @Test
